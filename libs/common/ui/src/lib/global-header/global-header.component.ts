b2d998dc3f469f25a5115aa8977ca981404f5119
Charlie Greenman <charlie@razroo.com>
2023-08-21 18:58:12 -0700

    Add code for angular-16.1.0 header-component

---
+++ libs/common/ui/src/lib/global-header/global-header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, NgModule, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faBell, faQuestionCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'razroo-nuxt-starter-global-header',
+  templateUrl: './global-header.component.html',
+  styleUrls: ['./global-header.component.scss'],
+})
+export class GlobalHeaderComponent implements OnInit {
+  faUserCircle = faUserCircle;
+  faQuestionCircle = faQuestionCircle;
+  faBell = faBell;
+  @Output() sideNavToggle: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  emitSideNavToggle() {
+    this.sideNavToggle.emit();
+  }
+}
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    FontAwesomeModule
+  ],
+  declarations: [GlobalHeaderComponent],
+  exports: [GlobalHeaderComponent],
+})
+export class GlobalHeaderComponentModule {}
